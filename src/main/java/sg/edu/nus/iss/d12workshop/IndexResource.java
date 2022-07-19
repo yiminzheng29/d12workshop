5ff8bb801046a8c750dc4dc9def332ca473a5931
Yimin Zheng <yiminzheng29@gmail.com>
2022-07-19 11:30:37 +0800

    completed lecture  slide 17

--- src/main/java/sg/edu/nus/iss/d12workshop/IndexResource.java
+++ src/main/java/sg/edu/nus/iss/d12workshop/IndexResource.java
@@ -1,5 +1,8 @@
 package sg.edu.nus.iss.d12workshop;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,10 @@ public class IndexResource {
 
     @GetMapping(produces = {"text/html"})
     public String index(Model model) {
+        model.addAttribute("currTime", (new Date()).toString());
+
+        Calendar cal = Calendar.getInstance();
+        model.addAttribute("currHour", cal.get(Calendar.HOUR_OF_DAY));
         return "indexresource";
 
     }
