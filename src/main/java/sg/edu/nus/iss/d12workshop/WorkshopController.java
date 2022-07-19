f87484f3645d0ce0ae93acb5c99df7cc4e30a9be
Yimin Zheng <yiminzheng29@gmail.com>
2022-07-19 16:26:46 +0800

    workshop task 2 completed

--- src/main/java/sg/edu/nus/iss/d12workshop/WorkshopController.java
+++ src/main/java/sg/edu/nus/iss/d12workshop/WorkshopController.java
@@ -1,7 +1,11 @@
 package sg.edu.nus.iss.d12workshop;
 
+import java.util.*;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WorkshopController {
@@ -9,5 +13,31 @@ public class WorkshopController {
     @RequestMapping(path={ "/workshop"})
     public String test() {
         return "workshop";
+    }
+
+    @RequestMapping(path={"/processNumber"})
+    public String testResults(
+    @RequestParam(name="inputNumber", defaultValue = "1") Integer unit,
+        Model model
+    ) {
+        List<Integer> lstInt = new ArrayList<>();
+        int loopValue = Integer.parseInt(unit.toString());
+
+        int loop =1;
+        while (loop<=loopValue) {
+            int result = (int) (Math.random()* (loopValue)+1);
+            if (!lstInt.contains(Integer.valueOf(result))) {
+                lstInt.add(Integer.valueOf(result));
+                loop++;
+            }
+        }
+
+        for (int index = 0; index < lstInt.size(); index++) {
+            System.out.print("==> " + lstInt.get(index));
+            System.out.println();
+        }
+        model.addAttribute("number", lstInt); // inside workshopresult.html
+
+        return "workshopresult"; 
     }
 }
